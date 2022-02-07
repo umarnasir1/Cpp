182b5a8c6c17c2e8915686985ab6660dc724fd35
umarnasir1 <umar021@gmail.com>
2022-02-07 16:12:03 +0500

    Updated definition of Logic Errors

--- Basics/004LogicErrors.cpp
+++ Basics/004LogicErrors.cpp
@@ -1,13 +1,13 @@
 /*
 Logic Erros
 Errors or bugs in your code that cause your program to run incorrectly.
-Logic Errors are mistakes (careless mistakes, or sometimes the mistakes are if the programmer have incomplete or incorrect information) made by the programmer.
+Logic Errors are mistakes (careless mistakes, or sometimes the mistakes are if the programmer have incomplete or incorrect information) made by the programmer that cause a program to produce incorrect results.
 */
 
 // Example - Suppose we have a program that determines if a person can vote in an election and you must be 18 years or older to vote.
 
 /* In below sample code, the condition shoulld be if (age >= 18) or if (age > 17)
-As the program is now, it means that if you are 18, you cannot vote 
+As the program is now, it means that if you are 18, you cannot vote
 */
 
 if (age > 18) {
