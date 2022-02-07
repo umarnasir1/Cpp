63d5337364868e3ba653ab0f466558cdf2fe8915
umarnasir1 <umar021@gmail.com>
2022-02-07 15:39:31 +0500

    Organization

--- Basics/001helloworld.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream> // input, output libraries
-
-int main() {
-  std::cout << "Hellow World!!" << std::endl; //endl will flush the buffers and will move the cursor to the next line
-  return 0;
-}
