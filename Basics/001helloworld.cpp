bc7e5c767aa70da8fe4fb037f256143e18190198
umarnasir1 <umar021@gmail.com>
2022-02-07 14:46:33 +0500

    Adding comments

--- Basics/001helloworld.cpp
+++ Basics/001helloworld.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
+#include <iostream> // input, output libraries
 
 int main() {
-  std::cout << "Hellow World!!" << std::endl;
+  std::cout << "Hellow World!!" << std::endl; //endl will flush the buffers and will move the cursor to the next line
   return 0;
 }
