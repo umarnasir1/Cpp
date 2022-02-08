144f32b2182402824bcc1fe1b27b39d5d57950c0
umarnasir1 <umar021@gmail.com>
2022-02-08 23:52:20 +0500

    Case3: Qualified using namespace variant.

---
+++ Code/004Namespace3.cpp
@@ -0,0 +1,27 @@
+/*
+  3. Qualified using namespace variant.
+
+  Create a C++ program that asks the user for their favourite number between 1and 100.
+  Then read this number from the console
+
+  Suppose the user enters 24.
+
+  Then display the following to the console:
+  Amazing!! That's my favorite number too!
+  No really!!, 24 is my favorite number!
+*/
+
+#include <iostream> // inputs and outputs (cin, cout)
+
+using std::cout; // use only what you need
+using std::cin;
+using std::endl;
+
+int main(){
+  int favorite_number; // variable declaration
+  cout << "Enter your favorite number between 1 to 100: ";
+  cin >> favorite_number;
+  cout << "Amazing!! That's my favorite number too!" << endl
+       << "No really!!, " << favorite_number << " is my favorite number!" << endl;
+  return 0;
+}
