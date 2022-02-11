40e1fde0e446e3880e32e7d39cf51eb2ea91db3b
umarnasir1 <umar021@gmail.com>
2022-02-11 13:26:36 +0500

    (1) Adding problem with 2 rooms; (2) Using variables

--- Code/016ConstantsDeclared.cpp
+++ Code/016ConstantsDeclared.cpp
@@ -1,32 +1,27 @@
-// Constants - Declared constants
+// Constants - Declared constants- but with additional small room
 
 /*
     Frank's Carpet Cleaning Service
-    Charges $30 per room
+    Charges:
+      $25 per small room
+      $35 per large room
     Sales tax rate is 6%
     Estimates are valid for 30 days
 
-    Prompt the user for the number of rooms they would like cleaned
+    Prompt the user for the number of small and large rooms they would like cleaned
     and provide an estimate such as:
 
 Estimate for carpet cleaning service:
-Number of rooms: 2
-Price per room: $30
-Cost: $60
-Tax: $3.6
+Number of small rooms: 3
+Number of large rooms: 1
+Price per small room: $25
+Price per large room: $35
+Cost: $110
+Tax: $6.6
 ====================================
-Total estimate: $63.6
+Total estimate: $116.6
 This estimate is valid for 30 days
 
-Pseudocode:
-    Prompt the user to enter the number of rooms
-    Display number of rooms
-    Display price per room
-
-    Display cost:  (number of rooms * price per room)
-    Display tax:   number of rooms * price per room * tax rate
-    Display total estimate: (number of rooms * price per room) + (number of rooms * price per room * tax rate)
-    Display estimate expiration time
 */
 
 #include <iostream>
@@ -37,22 +32,39 @@ using std::endl;
 
 int main() {
     cout << "Hello, welcome to Frank's Carpet Cleaning Service" << endl;
-    cout << "\nHow many rooms would you like cleaned? ";
 
-    int number_of_rooms {0};
-    cin >> number_of_rooms;
+    int number_of_small_rooms {0};
+    cout << "\nHow many small rooms would you like cleaned? ";
+    cin >> number_of_small_rooms;
 
-    const double  price_per_room {30};
+    int number_of_large_rooms {0};
+    cout <<"\nHow many large rooms would you like cleaned? ";
+    cin >> number_of_large_rooms;
+
+    // Declared Constants
+    const double price_per_small_room {25.0};
+    const double price_per_large_room {35.0};
     const double sales_tax {0.06};
     const int estimate_expiry {30}; // days
 
+    // Variables
+    double cost = (price_per_small_room * number_of_small_rooms) +
+                  (price_per_large_room * number_of_large_rooms);
+    double tax = cost * sales_tax;
+
     cout << "\nEstimate for carpet cleaning service" << endl;
-    cout << "Number of rooms: " << number_of_rooms << endl;
-    cout << "Price per room: $" << price_per_room << endl;
-    cout << "Cost : $" << price_per_room * number_of_rooms << endl;
-    cout << "Tax: $" << price_per_room * number_of_rooms * sales_tax << endl;
+    cout << "Number of small rooms: " << number_of_small_rooms << endl;
+    cout << "Number of large rooms: " << number_of_large_rooms << endl;
+    cout << "Price per small room: $" << price_per_small_room << endl;
+    cout << "Price per large room: $" << price_per_large_room << endl;
+
+    cout << "Cost : $" << cost << endl;
+
+    cout << "Tax: $" << tax << endl;
+
     cout << "===============================" << endl;
-    cout << "Total estimate: $" << (price_per_room * number_of_rooms) + (price_per_room * number_of_rooms * sales_tax) << endl;
+    cout << "Total estimate: $" << cost + tax << endl;
+
     cout << "This estimate is valid for " << estimate_expiry << " days" << endl;
 
     cout << endl;
