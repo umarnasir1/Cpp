eb52ddb7a370e0ceda8032a349aae20209f82f75
umarnasir1 <umar021@gmail.com>
2022-02-14 18:34:27 +0500

    Initial Commit

---
+++ Basics/005ExpressionAndStatement.cpp
@@ -0,0 +1,32 @@
+/*
+  Expression & Statements 
+*/
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(){
+
+  // Eg of Expressons
+  34 // Literal
+  favorite_number // variable
+  1.5 + 2.8 // addition
+  2 * 5 // multiplication
+  a > b // relational
+  a = b // assignment
+
+  // E.g of Statements
+  int x; // declaration
+  favorite_number = 12; // assignment
+  1.5 + 2.8 // expression
+  x = 2 * 5 // assignment
+  if (a > b) cout << "a is greater than b"; // if
+  a = b // assignment
+  ; // null statement and performs no action
+
+
+  return 0;
+}
