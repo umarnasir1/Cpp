e069cee5dbf64d86116950fb08b3f264742fa8fd
umarnasir1 <umar021@gmail.com>
2022-02-17 11:09:21 +0500

    Added Example.

--- Code/031OperatorPrecedence.cpp
+++ Code/031OperatorPrecedence.cpp
@@ -4,6 +4,9 @@
 
 #include <iostream>
 
+using std:: cout;
+using std:: endl;
+
 int main(){
 
   /*
@@ -11,16 +14,23 @@ int main(){
     The precedence chart tells us that the multiplication operator has the highest precedence, followed by addition, followed by assignment. The statement is re-written on following line showing order of evaluation by including Paranthesis.
     Note that the evaluation takes place in order of multiplication, addition and finally, assignment.
   */
-  result = num1 + num2 * num3;
-  result = (num1 + (num2 * num3));
+  //result = num1 + num2 * num3;
+  //result = (num1 + (num2 * num3));
 
   /*
     Eg. 2: We again have three operators: the assignment operator, the addition operator and the subtraction operator. In this case, addition and subtraction are of higher precedence than assignment, but they both have the same precedence.
     So in this case, we need to use their associativity to determine the order of evaluation.
     We see that plus and minus associate left to right. So, this example using parentheses to show the order of evaluation is re-written below.
   */
-  result1 = num1 + num2 - num3;
-  result1 = ( (num1 + num2) - num3);
+  //result1 = num1 + num2 - num3;
+  //result1 = ( (num1 + num2) - num3);
+
+  // Eg.
+  int x {5};
+  int y {-2};
+  int z {2};
+
+  cout << (x + y * z <= x + z * z - x) << endl; // 1 - This evaluates to (1 <= 4) which is true, so 1 is displayed since 1 is true.
 
   return 0;
 }
