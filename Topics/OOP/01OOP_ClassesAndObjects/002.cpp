5bf8d242548bbbad26a9ece50eb7001cb95ada29
umarnasir1 <umar021@gmail.com>
2022-02-23 23:21:43 +0500

    Initial Commit.

---
+++ Topics/OOP/01OOP_ClassesAndObjects/002.cpp
@@ -0,0 +1,60 @@
+/*
+  Where do we create the player class?
+  If a class is created right inside main(), then its scope will be inside of main(), which means that only main will have access to that class.
+  If we would like the entire application to have access of the class, we would declare the class above main().
+  OBS!! Better way: Use separate files to declare classes.
+*/
+#include<iostream>
+#include<string>
+#include<vector>
+
+using std::string;
+
+// modeling a player depending on the problem we have.
+// Class Player will be a blueprint to create player obejcts from & every player object is going to have a string that represent their name, two integers; one representing their health and one their xp.
+// it is very powerful, as we are creating our own user-defined types, which are what players are and then we use it just like we can use any other variables.
+class Player {
+  // attributes - data
+  string name {"Player"}; // (This can be initilized using constructors also )
+  int health {100}; // 0-100; 0=dead (This can be initilized using constructors also )
+  int xp {3}; // Experience (This can be initilized using constructors also )
+
+  // methods - operations or functions - now they are prototypes and they are not defined yet - functions will be implimented them in later. We will get linker error, if we try to call them (without defining them)
+  void talk (string); // whatever string we pass into the player, the player will talk it right back out.
+  bool is_dead(); // This is a method that returns true or false based on whether the player is dead. And the player might be dead if their health is 0 or whatever other criteria we have for that.
+};
+
+class Account {
+  // attributes
+  string name {"Account"}; //name of the account initilized to account (This can be initilized using constructors also )
+  double balance {0.0}; //(This can be initilized using constructors also )
+
+  // Methods
+  bool deposit {double}; // method declaration - deposit some amount and return true/false weather it was successful or not.
+  bool withdraw {double}; // method declaration - withdraw some amount and return true/false depending on wheather it was successful.
+};
+
+int main(){
+  Account frank_account;
+  Account jim_account;
+
+  // creating player objects - each of player object will have player attribues and each can call defined methods in player class.
+  Player frank;
+  Player hero;
+
+  // array
+  // once we have created objects we can treat them like any variables. (like int and double.)
+  Player players [] {frank, hero}; // player is an array of player objects (initilized to frank and hero).
+
+  // vector of player - collection of player objects
+  vector <Player> playe_vec {frank};
+  player_vec.push_back{hero};
+
+
+  Player *enemy {nullptr}; // create objects on the heap, enemy is a pointer to a player. (remember, you read these from right to left) and we set it to null pointer.
+  enemy = new Player; // creating enemy object on the heap (the enemy object which is a player) and storing the address of that object in enemy, which is a pointer.
+
+  delete enemy; // when done delete enemy.
+
+  return 0;
+}
