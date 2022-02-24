8ac60ee0883acdb7f1704a4ab3bc8702c81e6d93
umarnasir1 <umar021@gmail.com>
2022-02-24 22:50:03 +0500

    Initial Commit.

---
+++ Topics/Basic/06ControllingProgramFlow/013dowhileloop.cpp
@@ -0,0 +1,88 @@
+/*
+  do-while loop
+
+  do {
+    statements;
+  } while (expression);
+
+*/
+
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::cin;
+using std::vector;
+
+int main (){
+
+  // ---- Eg.1
+  // input validation - ask the user to enter a number between 1 and 5. If the user enter out of range number, promt user to re-enter the number till user enters the number in the range.
+
+  int number {};
+
+  do {
+    cout << "Enter an integer between 1 and 5: ";
+    cin >> number;
+  } while(number <= 1 || number >= 5);
+
+  cout << "Thanks" << endl;
+
+  // ---- Eg.2
+  // area calculation
+  char selection {};
+
+  do {
+    double width {}, height {};
+    cout << "Enter width and height separated by a space: ";
+    cin >> width >> height;
+
+    double area {width * height};
+    cout << "The area is " << area << endl;
+
+    cout << "Calculate another ? (Y/N) : ";
+    cin >> selection;
+
+  } while(selection == 'Y' || selection == 'y'); // if the selection is any other character, the loop terminates.
+
+  cout << "Thanks!" << endl;
+
+  // ---- Eg.3
+  // Simple Menu
+  // do display the Menu
+  // while a user doesnt quit
+  char selection {};
+
+  do {
+    // Menu
+    cout << "\n------------------------------- \n"
+         << "1. Task 1\n"
+         << "2. Task 2\n"
+         << "3. Task 3\n"
+         << "Q. Quit" << endl;
+
+    cout << "\nEnter your selection: ";
+    cin >> selection;
+
+    switch (selection) {  // can also work with if - else if - else if - else if -else
+      case '1':
+        cout << "Operations for Task 1" << endl;
+        break;
+      case '2':
+        cout << "Operations for Task 2" << endl;
+        break;
+      case '3':
+        cout << "Operations for Task 3" << endl;
+        break;
+      case 'q':
+      case 'Q':
+        cout << "Thank you!" << endl;
+      default:
+        cout << "Invalid Entry. Please try again" << endl;
+    }
+
+  } while(selection != 'Q' && selection != 'q');
+
+  return 0;
+}
