0cdbfd30a988882f00f31c3e4c9a6d13175fd660
umarnasir1 <umar021@gmail.com>
2022-03-04 13:28:01 +0500

    Added Eg 4: Grocery List.

--- Topics/Basic/08Functions/006DefaultArguments.cpp
+++ Topics/Basic/08Functions/006DefaultArguments.cpp
@@ -20,8 +20,10 @@
    Eg 1 Single Default argument
   // if 96% of our customers will have 6% tax rate so we can make that as default value and only supply argument when tax rate is not 6%
 
-  Eg 2 - Multiple default arguments
-  Eg 3 - Eg with strings
+  Eg.2 - Multiple default arguments
+  Eg.3 - Multiple default arguments - Eg with strings
+  Eg.4 - Multiple default arguments - Grocery List
+    Automatically print Grocery list. Most weeks the grocery list is the same.
 */
 
 #include<iostream>
@@ -34,12 +36,14 @@ using std::cout;
 using std::endl;
 using std::string;
 
-// prototype
+// Function Prototype
 double calc_cost(double base_cost, double tax_rate = 0.06);
 
 double calc_cost1 (double base_cost1 = 100, double tax_rate1 = 0.06, double shipping1 = 3.50);
 
 void greeting(string name, string prefix = "Mr.", string suffix = "");
+
+void print_grocery_list (int apples = 3, int oranges = 7, int mangos = 13);
 
 int main (){
 
@@ -69,7 +73,7 @@ int main (){
   cost1 = calc_cost1();
   cout << "Cost is: " << cost1 << endl; // 109,50
 
-  // Eg. 03 Multiple default arguments.
+  // Eg. 03 Multiple default arguments - Strings
   cout << "\n--- Eg. 03 ------------------------------" << endl;
   greeting("Glenn Jones", "Dr.", "M.D.");
   greeting("James Rogers", "Professor", "Ph.D");
@@ -77,9 +81,16 @@ int main (){
   greeting("William Smith"); // default prefix and suffix
   greeting("Mary Howard", "Mrs.", "Ph.D");
 
+  // Eg. 04 Multiple default arguments - Grocery List
+  cout << "\n--- Eg. 04 ------------------------------" << endl;
+  print_grocery_list(); // Week 1
+  print_grocery_list(5); // Week 2 - 5 apples instead of 3 and others as usual . 
+  print_grocery_list(7, 11); // Week 3 - having a party and would like to serve fruit punch so 7 apples are nned and 11 oranges along with default mangos.
+
   return 0;
 }
 
+// Function Definitions
 // The function calculates the tax amount and adds that to the base costs and returns that value to the caller.
 double calc_cost (double base_cost, double tax_rate){
   return base_cost += (base_cost * tax_rate);
@@ -91,4 +102,10 @@ double calc_cost1 (double base_cost1, double tax_rate1, double shipping1){
 
 void greeting(string name, string prefix, string suffix){
   cout << "Hello " << prefix + " " + name + " " + suffix << endl;
+}
+
+void print_grocery_list (int apples, int oranges, int mangos){
+  cout << "Apples: " << apples << endl
+       << "Oranges: " << oranges << endl
+       << "Mangoes: " << mangos << endl;
 }
