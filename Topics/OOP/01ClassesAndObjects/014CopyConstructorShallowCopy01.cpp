c8e78f6605eca680b11e63818313786c43010ba1
umarnasir1 <umar021@gmail.com>
2022-04-08 01:54:14 +0500

    (1) Added Constructor; (2) Renamed.

--- Topics/OOP/01ClassesAndObjects/014CopyConstructorShallowCopy01.cpp
+++ Topics/OOP/01ClassesAndObjects/014CopyConstructorShallowCopy01.cpp
@@ -1,12 +1,6 @@
 /*
-  How can we create a copy of object?
-  Copy Constructor
-    when an object of a class is created and is initialized by object of same class.
-  Default Assignment Operator / Implicit Copy Assignment Operator
-    Class_name object1;
-    Class_name object2;
-    object2= object1;
-
+  Shallow Copy
+    Only the pointer is copied - not what is pointing to!
 */
 #include<iostream>
 
@@ -19,9 +13,14 @@ private:
 public:
   void set_data_value(int val1, int val2){ a = val1; b = val2;}
   void show_data(){cout << a << " , " << b << endl;}
-  //Copy Constructor
-  Dummy(const Dummy &source); //prototype
+  Dummy(int v1=0, int v2=0); //Constructor
+  Dummy(const Dummy &source); //Copy Constructor prototype
 };
+
+// Constructor
+Dummy::Dummy(int v1, int v2)
+  : a{v1}, b{v2}{
+}
 
 // Copy Constructor
 Dummy::Dummy(const Dummy &source)
@@ -31,6 +30,7 @@ Dummy::Dummy(const Dummy &source)
 
 int main(){
   Dummy d1;
+  d1.show_data();
   d1.set_data_value(3,4);
   d1.show_data();
   Dummy d2{d1}; // initializing d2 to d1 - copying values of d1 to d2
