a5ab5f708e90fd06adfd757464a9872857d5db55
umarnasir1 <umar021@gmail.com>
2022-05-07 22:21:59 +0500

    Printed Title and Headers.

--- Topics/IntermediateAdvance/IOAndStreams/005.cpp
+++ Topics/IntermediateAdvance/IOAndStreams/005.cpp
@@ -1,84 +1,100 @@
-/**/
+/*
+*/
 #include<iostream>
-#include<iomanip>
 #include<string>
+#include<iomanip>
 #include<vector>
 
-void ruler ();
+void ruler(); // Function Prototype
 
-struct City {
-    std::string name;
-    long population;
-    double cost;
+struct City{
+  std::string name;
+  long population;
+  double cost;
 };
 
-// Assume each country has at least 1 city
-struct Country {
-    std::string name;
-    std::vector<City> cities;
+struct Country{
+  std::string name;
+  std::vector<City> cities;
 };
 
-struct Tours {
-    std::string title;
-    std::vector<Country> countries;
+struct Tours{
+  std::string title;
+  std::vector<Country> countries;
 };
 
-int main()
-{
+int main(){
 
-  Tours tours
-      { "Tour Ticket Prices from Miami",
-          {
-              {
-                  "Colombia", {
-                      { "Bogota", 8778000, 400.98 },
-                      { "Cali", 2401000, 424.12 },
-                      { "Medellin", 2464000, 350.98 },
-                      { "Cartagena", 972000, 345.34 }
-                  },
-              },
-              {
-                  "Brazil", {
-                      { "Rio De Janiero", 13500000, 567.45 },
-                      { "Sao Paulo", 11310000, 975.45 },
-                      { "Salvador", 18234000, 855.99 }
-                  },
-              },
-              {
-                  "Chile", {
-                      { "Valdivia", 260000, 569.12 },
-                      { "Santiago", 7040000, 520.00 }
-              },
-          },
-              { "Argentina", {
-                  { "Buenos Aires", 3010000, 723.77 }
-              }
-          },
-      }
-  };
+  Tours tours{    //Loading Data into the struc - Uniform Initialization - initializer list to initialize structure (Modern C++)
+    "Tour Ticket Prices from Miami", {
+      {
+        "Colombia", {
+          {"bogota", 8778000, 400.98}, // city
+          { "Cali", 2401000, 424.12 },
+          { "Medellin", 2464000, 350.98 },
+          { "Cartagena", 972000, 345.34 }
+        } // Country - City
+      },// Country - Colombia
+      {
+        "Brazil", {
+          {"Rio De janiero", 13500000, 567.45},
+          { "Sao Paulo", 11310000, 975.45 },
+          { "Salvador", 18234000, 855.99 }
+        } // Country -City
+      }, // Country - Brazil
+      {
+        "Chile", {
+          { "Valdivia", 260000, 569.12 },
+          { "Santiago", 7040000, 520.00 }
+        } // Country - City
+      }, // Country - Chile
+      {
+        "Argentina", {
+          { "Buenos Aires", 3010000, 723.77 }
+        } // Country - City
+      } // Country - Argentina
+    }// Tours - Country
+  }; //Tours
 
-  // Unformatted display so you can see how to access the vector elements
+  const int total_width {70};
+  const int field1_width {20}; // Country
+  const int field2_width {20}; // City
+  const int field3_width {15}; // Population
+  const int field4_width {15}; // Price
+
   ruler();
-  std::cout << std::setw(50) << tours.title << std::endl;
-  std::cout << std::setw(20) << std::left << "Country"
-            << std::right << "City"
-            << std::setw(31) << std::right << "Population" << std::endl;
-  std::cout << "----------------------------------------------------------------------\n";
-  for(auto country : tours.countries) {   // loop through the countries
-      std::cout << country.name << std::endl;
-      for(auto city : country.cities) {       // loop through the cities for each country
-          std::cout << "\t" << city.name
-                        << "\t" << city.population
-                        << "\t" << city.cost
-                        << std::endl;
+  int title_length = tours.title.length(); // 29
+  // std::cout << ((total_width - title_length)/2) << std::endl; //20
+  // std::cout << std::setw((total_width - title_length)/2) << "ABCD" << std::endl; // 14
+  std::cout << std::setw((total_width - title_length)/2) << "" << tours.title << std::endl; // "" - empty string - setw() will apply to empty string and so trours.title will be displayed after that.
+  std::cout << std::endl;
+  std::cout << std::setw(field1_width) << std::left << "Country"
+            << std::setw(field2_width) << std::left << "City"
+            << std::setw(field3_width) << std::right << "Population"
+            << std::setw(field4_width) << std::right << "Price"
+            << std::endl;
+
+  std::cout << std::setw(total_width)
+            << std::setfill('-')
+            << "" // setw() and setfill() will apply to empty string
+            << std::endl; // display total_width dashes
+
+  for(Country country : tours.countries) {
+      for (size_t i = 0; i<country.cities.size(); ++i) {
+          std::cout << std::setw(field1_width) << std::left <<  ((i == 0) ? country.name : "") // conditional operator
+                         << std::setw(field2_width) << std::left << country.cities.at(i).name
+                         << std::setw(field3_width) << std::right<< country.cities.at(i).population
+                         << std::setw(field4_width) << std::right << country.cities.at(i).cost
+                         << std::endl;
       }
   }
 
-  std::cout << std::endl << std::endl;
+
+
   return 0;
 }
 
-// Function Definition
-void ruler (){
-  std::cout << "\n1234567890123456789012345678901234567890123456789012345678901234567890\n\n";
+// Function Description
+void ruler() {
+    std::cout << "\n1234567890123456789012345678901234567890123456789012345678901234567890\n" << std::endl;
 }
