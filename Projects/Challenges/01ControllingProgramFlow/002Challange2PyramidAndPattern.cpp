2883fd539d6318b6fd8ec4fd6886902ba215f8cf
umarnasir1 <umar021@gmail.com>
2022-07-02 15:20:50 +0500

    Program to print half pyramid using alphabets.

--- Projects/Challenges/01ControllingProgramFlow/002Challange2PyramidAndPattern.cpp
+++ Projects/Challenges/01ControllingProgramFlow/002Challange2PyramidAndPattern.cpp
@@ -1,3 +1,4 @@
+// Example 1:
 // Program to print half pyramid using *
 
 // #include<iostream>
@@ -6,7 +7,7 @@
 //
 //   int rows {};
 //
-//   std::cout << "Enter number of rows : ";
+//   std::cout << "Enter number of rows: ";
 //   std::cin >> rows;
 //
 //   for (int i{1}; i <= rows; i++){ // rows
@@ -18,19 +19,41 @@
 //   return 0;
 // }
 
+// Example 2:
 // Program to print pyramid using numbers
+// #include<iostream>
+//
+// int main(){
+//
+//   int rows {};
+//
+//   std::cout << "Enter number of rows: ";
+//   std::cin >> rows;
+//
+//   for (int i{1}; i <= rows; i++){ // rows
+//     for (int j{1}; j <= i; j++) // cols
+//       std::cout << j << " ";
+//     std::cout << std::endl;
+//   }
+//
+//   return 0;
+// }
+
+// Example 3:
+// Program to print half pyramid using alphabets
 #include<iostream>
 
 int main(){
 
-  int rows {};
+  char alphabet = 'A', user_input;
 
-  std::cout << "Enter number of rows : ";
-  std::cin >> rows;
+  std::cout << "Enter the uppercase character to be printed in the last row: ";
+  std::cin >> user_input;
 
-  for (int i{1}; i <= rows; i++){ // rows
+  for (int i{1}; i <= (user_input-'A'+1); i++){ // rows - https://en.cppreference.com/w/cpp/language/ascii, iterations -user_input-'A'+1
     for (int j{1}; j <= i; j++) // cols
-      std::cout << j << " ";
+      std::cout << alphabet << " ";
+    ++alphabet;
     std::cout << std::endl;
   }
 
