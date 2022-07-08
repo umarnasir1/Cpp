838f82f6e21d14c66ea9cf719ed0309d17311bb8
umarnasir1 <umar021@gmail.com>
2022-07-08 16:57:46 +0500

    (1) Updated Description; (2) Added two (2) Altrnative Solution.

--- Projects/Challenges/02CharactersAndStrings/002Challenge2LetterPyramid.cpp
+++ Projects/Challenges/02CharactersAndStrings/002Challenge2LetterPyramid.cpp
@@ -78,9 +78,29 @@
   Which C++ loop(s) did you use and why?
   How did you handle displaying the leading spaces in each row of the pyramid?
   Now that you completed the assignment, how might approach the problem differently if you had to solve it again?
+
+  Loops Used: 
+  (1) For loop: used as main loop to construct a pyramid and another for loop is used inside main loop to deal with leading spaces in each row of the pyramid. (for loop selected here as its more efficient then while loop here. The same thing can be done using while loop but it will require few extra lines of code) 
+  (2) While loop: used inside the main for loop to display the string elements. The same logic can be implemented using a for loop.
+
+  Leading Spaces in each row of the pyramid are handled in a separate for loop.
+
+  Different approach to problem: A different approach can be to split the pyramid into three parts, till center, center, reverse order and to loop it accordingly to display characters.
+*/
+
+/*
+  Solution
+    The idea when solving this problem is first to break up the problem up into subproblems.
+    Get the input from the user and store it in a std::string variable
+    Loop for each row
+        Determine how many blank spaces must be displayed before the current row and display them
+        Determine how many letters must be displayed and display them
+        Display the remaining characters in reverse order
+        Complete the row with a new line
 */
 
 #include <iostream>
+#include <string>
 
 int main (){
   std::string user_input;
@@ -115,3 +135,102 @@ int main (){
   }
   return 0;
 }
+
+/*
+  Alternative Solution 1- Course Instructor
+  The idea when solving this problem is first to break up the problem up into subproblems.
+  Get the input from the user and store it in a std::string variable
+  Loop for each letter in the string entered by the user
+      Determine how many blank spaces must be displayed before the current row and display them
+      Determine how many letters must be displayed before the current character and display them
+      Display the current character
+      Display the remaining characters in reverse order
+      Complete the row with a new line
+
+  There are many, many ways to solve this problem. Solution is considering that the std::string .length() and .at() methods use size_t as the type for the return value and parameter.
+  int can be used instead of size_t for this problem, but you will get compiler warnings about int and size_t compatibility issues.
+*/
+// Letter Pyramid
+// Written by Frank J. Mitropoulos
+
+// #include <iostream>
+// #include <string>
+//
+// int main()
+// {
+//     std::string letters{};
+//
+//     std::cout << "Enter a string of letters so I can create a Letter Pyramid from it: ";
+//     getline(std::cin, letters);
+//
+//     size_t num_letters = letters.length();
+//
+//     int position {0};
+//
+//     // for each letter in the string
+//     for (char c: letters) {
+//
+//         size_t num_spaces = num_letters - position; // Paramid stats with a " " thats y one extra space here
+//         while (num_spaces > 0) {
+//             std::cout << " ";
+//             --num_spaces;
+//         }
+//
+//         // Display in order up to the current character
+//         for (size_t j=0; j < position; j++) {
+//             std::cout << letters.at(j);
+//         }
+//
+//         // Display the current 'center' character
+//         std::cout << c;
+//
+//         // Display the remaining characters in reverse order
+//         for (int j=position-1; j >=0; --j) {
+//             // You can use this line to get rid of the size_t vs int warning if you want
+//             auto k = static_cast<size_t>(j);
+//             std::cout << letters.at(k);
+//         }
+//
+//         std::cout << std::endl; // Don't forget the end line
+//         ++position;
+//     }
+//
+//     return 0;
+// }
+
+// Alternative Solution 2
+// #include <iostream>
+// #include <string>
+//
+// using namespace std;
+//
+// int main() {
+//
+//     cout << "Enter a string: ";
+//     string aString;
+//     getline(cin, aString);
+//
+//     for (int i {0}; i < aString.length(); i++)
+//     {
+//
+//         for(int j {0}; j < aString.length() - i - 1; j++)
+//         {
+//             cout << " ";
+//         }
+//         for(int j {0}; j <= i; j++)
+//         {
+//             cout << aString[j];
+//         }
+//         for(int j {i-1}; j > -1; j--)
+//         {
+//             cout << aString[j];
+//         }
+//         cout << endl;
+//     }
+//
+//
+//
+//     return 0;
+//
+//
+// }
