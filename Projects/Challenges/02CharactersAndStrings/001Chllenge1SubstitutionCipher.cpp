0a57fe3c9b842398aee6230569f31a3b8a925679
umarnasir1 <umar021@gmail.com>
2022-07-08 18:43:37 +0500

    Updated body.

--- Projects/Challenges/02CharactersAndStrings/001Chllenge1SubstitutionCipher.cpp
+++ Projects/Challenges/02CharactersAndStrings/001Chllenge1SubstitutionCipher.cpp
@@ -28,13 +28,42 @@ Reuse existing functionality in libraries and in the std::string class!
 */
 
 #include <iostream>
+#include <string>
+#include <vector>
 
 int main() {
 
-    std::string alphabet {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
-    std::string key {"XZNLWEBGJHQDYVTKFUOMPCIASRxznlwebgjhqdyvtkfuompciasr"};
+  std::string secret_message, encrypted_message, decrypted_message; // automatic initialization of C++ Strings.
 
+  std::string alphabet {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+  std::string key {"XZNLWEBGJHQDYVTKFUOMPCIASRxznlwebgjhqdyvtkfuompciasr"};
 
-    std::cout << std::endl;
-    return 0;
+  std::cout << "Enter your secret message: ";
+  std::getline(std::cin,secret_message);
+
+  std::cout << "\nEncrypting message..." << std::endl;
+
+  // Encryption
+  for (auto letter: secret_message)
+    // std::cout << alphabet << " " << letter << " " << alphabet.find(letter) << std::endl;
+    if (isalpha(letter))
+      encrypted_message += key.at(alphabet.find(letter)); //key[alphabet.find(letter)]; // finding the position of character in secret message in the key
+    else
+      encrypted_message += letter;
+
+  std::cout << "\nEncrypted message: " << encrypted_message << std::endl;
+
+  std::cout << "\nDecrypting message..." << std::endl;
+
+  // Decryption
+  for (auto letter: encrypted_message)
+    if (isalpha(letter))
+      decrypted_message += alphabet.at(key.find(letter)); // alphabet[key.find(letter)];
+    else
+      decrypted_message += letter;
+
+  std::cout << "\nDecrypted message: " << decrypted_message << std::endl;
+
+  std::cout << std::endl;
+  return 0;
 }
