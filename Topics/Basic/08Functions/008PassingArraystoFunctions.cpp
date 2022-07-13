b9d98d3450d934eaebd6f80846956054cd0486a1
umarnasir1 <umar021@gmail.com>
2022-07-13 08:24:42 +0500

    Updated comments.

--- Topics/Basic/08Functions/008PassingArraystoFunctions.cpp
+++ Topics/Basic/08Functions/008PassingArraystoFunctions.cpp
@@ -11,66 +11,60 @@
 
   // Eg03
   The scenario in eg 2 (modifying of array values), can be avoided by making the function parameters const (read-only) - within the function body).In this case any attempt to modify the array will result in compiler error.
-
-
 */
 
-#include<iostream>
-
-using std::cout;
-using std::endl;
+#include <iostream>
 
 // Function prototype
-void print_array(int numbers [], size_t size);
-void zero_array(int numbers [], size_t size);
-void set_array(int numbers [], size_t size, int value);
-void print_array1(const int numbers [], size_t size);
+void print_array(int arr[], size_t size);
+void zero_array(int arr[], size_t size);
+void set_array(int arr[], size_t size, int value);
+// void print_array1(const int numbers [], size_t size);
 
 int main (){
 
   // Eg. 01 - Displaying elements of Array
-  cout << "------------ Eg. 01 ------------------------------" << endl;
-  int my_numbers [] {1,2,3,4,5};
-  print_array(my_numbers, 5);
+  std::cout << "------------ Eg. 01 ------------------------------" << std::endl;
+  int my_scores[] {100, 98, 90, 86, 84};
+  print_array(my_scores, 5);
 
   // Eg. 02 -Since we are passing the location of the array, the function can modify the actual array.
-  cout << "\n------------ Eg. 02 ------------------------------" << endl;
-  zero_array(my_numbers, 5); // my_numbers is now zeros!
-  print_array(my_numbers, 5); // 0 0 0 0 0
-  set_array(my_numbers, 5, 100);
-  print_array(my_numbers, 5); // 100 100 100 100 100
-  print_array(my_numbers, 5); // 50000 100 100 100 100
+  std::cout << "\n------------ Eg. 02 ------------------------------" << std::endl;
+  zero_array(my_scores, 5); // my_numbers is now zeros!
+  print_array(my_scores, 5); // 0 0 0 0 0
+  set_array(my_scores, 5, 100);
+  print_array(my_scores, 5); // 100 100 100 100 100
+  print_array(my_scores, 5); // 50000 100 100 100 100 with uncommented line - after adding const, this array will not change.
 
-  cout << "\n------------ Eg. 03 ------------------------------" << endl;
-//  int my_numbers1 {6,7,8,9,10};
-//  print_array1(my_numbers, 5); // Compiler error as function parameter is const (read-only)
+  // std::cout << "\n------------ Eg. 03 ------------------------------" << std::endl;
+  // int my_numbers1 [] {6,7,8,9,10};
+  // print_array1(my_numbers1, 5); // Compiler error as function parameter is const (read-only)
 
+  std::cout << std::endl;
   return 0;
 }
 
 // Function Definition
-void print_array(const int numbers [], size_t size){ // int numbers [] will work too but const is just added to insure that array does not get modified (intentionally or unintentionally). if it trys to modify unitentionally, compiler error - check pring_array1 function.
-  for (size_t i{0}; i < size; ++i)
-    cout << numbers[i] << " ";
-  cout << endl;
-//  numbers[0]= 50000; // just to test ... print_array1 written for this puprose
+void print_array(int arr[], size_t size){// int arr [] will work too but const is just added to insure that array does not get modified (intentionally or unintentionally). if it trys to modify unitentionally, compiler error - check pring_array1 function. (print_array1 function.)
+  for (size_t i{0}; i<size; ++i)
+    std::cout << arr[i] << " ";
+  std::cout << std::endl;
+  // arr[0] = 50000; // just to test this will update the arr[] ... print_array1 written for this puprose
 }
 
-void zero_array(int numbers [], size_t size){ // yahan pe const hona chiye
-  for (size_t i{0}; i < size; ++i)
-    numbers[i] = 0;
+void zero_array(int arr[], size_t size){ // yahan pe const hona chiye
+  for (size_t i{0}; i<size; ++i)
+    arr[i] = 0;
 }
 
 // set each array element to a value
-void set_array(int numbers [], size_t size, int value){
+void set_array(int arr[], size_t size, int value){
   for (size_t i{0}; i < size; ++i)
-    numbers[i] = value;
+    arr[i] = value;
 }
 
-/*
-void print_array1(const int numbers [], size_t size){
-  for (size_t i{0}; i < size; ++i)
-    cout << numbers[i] << endl;
-  numbers[2] = 50000;
-}
-*/
+// void print_array1(const int numbers [], size_t size){
+//   for (size_t i{0}; i < size; ++i)
+//     std::cout << numbers[i] << std::endl;
+//   numbers[2] = 50000; // compiler error
+// }
