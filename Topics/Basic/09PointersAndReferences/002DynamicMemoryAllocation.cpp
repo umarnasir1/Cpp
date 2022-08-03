8bff8b4a4d5d3369f48379fc4248c7ce23e6ac62
umarnasir1 <umar021@gmail.com>
2022-08-03 16:38:13 +0500

    (1) Updating comments.

--- Topics/Basic/09PointersAndReferences/002DynamicMemoryAllocation.cpp
+++ Topics/Basic/09PointersAndReferences/002DynamicMemoryAllocation.cpp
@@ -3,7 +3,6 @@
 
   Single int, double, strings, vectors, ...   - new, delete
   Array (of int, ... ) - new[], delete[]
-
 */
 
 #include <iostream>
@@ -15,25 +14,28 @@ using std::endl;
 int main (){
 
   // Using new to allocate storage
-  int *int_ptr {nullptr}; // this is on stack (in main() function) point to the . this pointer will hold address of integers and now its pointing to nowhere (as initialized to nullptr)
+  int *int_ptr {nullptr}; // this is on stack (in main() function) point to the . this pointer will hold address of integers and now its pointing to nowhhere (as initialized to nullptr)
 
   int_ptr = new int;  // allocating int at runtime on the heap and allocating storage for an int on the heap and stores address in int_ptr. We are pointing to storage from int_ptr
 
-  cout << int_ptr << endl; // 0x7fe17f4057e0 -  memory location where that integer is on the heap. address that is stored in int_ptr and int_ptr is pointing to it.
+  cout << int_ptr << endl; // 0x7fe17f4057e0 -  memory location where that integer is on the heap. address that is stored in int_ptr and int_ptr is pointing to it. address of newly created integer.
 
-  cout << *int_ptr << endl; // 41188048 - garbage - Thats because the integer that was allocated hasent been initialized. So we will assign value to that integer on the heap
+  cout << *int_ptr << endl; // 41188048 - dereferencing pointer to check value of integer. garbage -  Thats because the integer that was allocated hasent been initialized.
 
-  *int_ptr = 100;
+  *int_ptr = 100; // assigning value to that integer on the heap
 
   cout << *int_ptr << endl; // 100
 
   // Using delete to deallocate storage
   delete int_ptr; // frees the allocaated storage
 
-  /////// Array
-  // Using new[] to allocate storage for an array
+  // --------------------------------
+  // Array
+  // --------------------------------
 
-  double *array_ptr {nullptr};
+  // Using new[] to allocate storage for an array dynamically (at run time)
+
+  double *array_ptr {nullptr}; // on stack
   size_t size {0};
 
   cout << "How big do you want the array? ";
@@ -41,9 +43,9 @@ int main (){
 
   // what happens if our call to allocate the size fails.
   //while (true) // endless loop
-  //  array_ptr = new double[size]; // keep allocating - and creating memory leaks leaks all over the place.  Eventually, we will run out of memory and heap 
+  //  array_ptr = new double[size]; // keep allocating - and creating memory leaks leaks all over the place.  Eventually, we will run out of memory and heap
 
-  array_ptr = new double[size]; // allocate array on the heap and stores the address of first one in array_ptr
+  array_ptr = new double[size]; // allocate array on the heap contiguously in the memory and stores the address of first one in array_ptr and the address of the first element is stored in array_ptr
 
   cout << array_ptr << endl; // 0x7f82ac504080- address of first double
 
