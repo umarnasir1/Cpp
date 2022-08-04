79092a53f624f11cd18f177790afa558b00c9bc5
umarnasir1 <umar021@gmail.com>
2022-08-04 20:56:09 +0500

    (1) Addign std::cout & std::endl; (2) Updating comments.

--- Topics/Basic/09PointersAndReferences/004PointerArithmetic.cpp
+++ Topics/Basic/09PointersAndReferences/004PointerArithmetic.cpp
@@ -11,20 +11,20 @@ using std::boolalpha;
 
 int main (){
 
-  int scores [] {100, 95, 89, 68, -1};
+  int scores [] {100, 95, 89, 68, -1}; // -1 used as sentinal value/terminator... so whenever we see -1, we can terminate the loop.
   int *score_ptr {scores}; // score_ptr is pointing to address of first element of array
 
   // loop through an array using a pointer and a sentinal value (-1)/terminator
   while (*score_ptr != -1){ // exit loop when value is -1
-    cout << *score_ptr << endl; // Dereferencing
-    score_ptr++; // increment
+    std::cout << *score_ptr << std::endl; // Dereferencing
+    score_ptr++; // increment (as its pointing to int, it will increment by 4 because we have 4 byte integer)
   }
 
   // Another way to do this
-  cout << "\n---------------------------------" << endl;
+  std::cout << "\n---------------------------------" << std::endl;
   score_ptr = scores;
   while (*score_ptr != -1)
-    cout << *score_ptr++ << endl; // dereference score_ptr and increment it  - *score_ptr++ has 2 operators, * (dereference operator) and ++ (postfix increment operator). Since we have 2 operators with different precedence (precedence of the postfix increment operator is greater than that of the dereference operator), this becomes equivalent to: *(score_ptr++). This means that we are dereferencing score_ptr, but we also know that we are incrementing score_ptr. Since the increment is a post-increment, then the effect of is: 1. *score_ptr 2. increment score_ptr
+    std::cout << *score_ptr++ << std::endl; // dereference score_ptr and increment it  - *score_ptr++ has 2 operators, * (dereference operator) and ++ (postfix increment operator). Since we have 2 operators with different precedence (precedence of the postfix increment operator is greater than that of the dereference operator), this becomes equivalent to: *(score_ptr++). This means that we are dereferencing score_ptr, but we also know that we are incrementing score_ptr. Since the increment is a post-increment, then the effect of is: 1. *score_ptr 2. increment score_ptr
     // OBS!!
     // int result {0}, value {1000};
     // result = (value++); This will assign 1000 to result then increment - the () doesnt matter
@@ -32,7 +32,7 @@ int main (){
 
 
   // Comparing two pointers
-  cout << "\n---------------------------------" << endl;
+  std::cout << "\n---------------------------------" << std::endl;
   string s1 {"Bjarne"};
   string s2 {"Bjarne"};
   string s3 {"Stroustrup"};
@@ -41,32 +41,32 @@ int main (){
   string *p2 {&s2};
   string *p3 {&s1};
 
-  cout << boolalpha;
-  cout << p1 << " == " << p2 << ": " << (p1 == p2) << endl; // false
-  cout << p1 << " == " << p3 << ": " << (p1 == p3) << endl; // true - as same memory location
+  std::cout << boolalpha; // so we can print True and False
+  std::cout << p1 << " == " << p2 << ": " << (p1 == p2) << std::endl; // false - not pointing to same address.
+  std::cout << p1 << " == " << p3 << ": " << (p1 == p3) << std::endl; // true - as same memory location
 
   // Comparing the data pointers are pointing to
-  cout << *p1 << " == " << *p2 << ": " << (*p1 == *p2) << endl; // True
-  cout << *p1 << " == " << *p3 << ": " <<  (*p1 == *p3) << endl; // True
+  std::cout << *p1 << " == " << *p2 << ": " << (*p1 == *p2) << std::endl; // True
+  std::cout << *p1 << " == " << *p3 << ": " <<  (*p1 == *p3) << std::endl; // True
 
   p3 = &s3; // point to Straustrup
-  cout << *p1 << " == " << *p3 << ": " << (*p1 == *p3) << endl; // False
+  std::cout << *p1 << " == " << *p3 << ": " << (*p1 == *p3) << std::endl; // False
 
   // Eg. using - (substracting two pointers - there are xx characters in between)
-  cout << "\n---------------------------------" << endl;
-  char name [] {"Bjarne"}; // array of characters B j a r n e \0
+  std::cout << "\n---------------------------------" << std::endl;
+  char name [] {"Bjarne"}; // array of characters B j a r n e \0  - C style string
 
   char *char_ptr1 {nullptr};
   char *char_ptr2 {nullptr};
 
-  char_ptr1 = &name[0]; // address of F - &name would be same
+  char_ptr1 = &name[0]; // address of B - &name would be same
   char_ptr2 = &name[3]; // address of r
 
-  cout << "In the string " << name << ", " << *char_ptr2 << " is " << (char_ptr2 - char_ptr1) << " characters away from " << *char_ptr1 << endl;
+  std::cout << "In the string " << name << ", " << *char_ptr2 << " is " << (char_ptr2 - char_ptr1) << " characters away from " << *char_ptr1 << std::endl;
 
-  // increment
-  int_ptr += n;
-  int_ptr -= n;
+  // // increment
+  // int_ptr += n;
+  // int_ptr -= n;
 
   return 0;
 }
