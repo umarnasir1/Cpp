1f82f76aa2e7301aed874a6eb92626accb4d3f66
umarnasir1 <umar021@gmail.com>
2022-12-06 18:54:22 +0500

    Updated Comments.

--- Projects/Challenges/04PointersAndReferences/001Challenge1/001Challenge1.cpp
+++ Projects/Challenges/04PointersAndReferences/001Challenge1/001Challenge1.cpp
@@ -38,8 +38,8 @@
 #include <iostream>
 
 // Function Prototypes
-void print(int array[], size_t array_size);
-int *apply_all(int arrayofinteger1 [], size_t size1, int arrayofinteger2[], size_t size2); 
+void print(const int array[], size_t array_size);
+int *apply_all(const int arrayofinteger1 [], size_t size1, const int arrayofinteger2[], size_t size2); 
 
 
 int main() {
@@ -56,37 +56,65 @@ int main() {
     print(array2,array2_size);
 
     int *results = apply_all(array1, array1_size, array2, array2_size);
-    constexpr size_t results_size {array1_size * array2_size};
+    constexpr size_t results_size {array1_size * array2_size}; // constant expression
 
     std::cout << "Result: ";
     print(results, results_size);
 
-    delete [] results; // delete storage on the HEAP
+    delete [] results; // free up the storage on the HEAP
     std::cout << std::endl;
 
     return 0;
 }
 
 // Function Definition
-void print(int array[], size_t array_size){
+
+/*******************************************************************
+    The function loops through arr and displays all the integers
+    in the array
+********************************************************************/
+
+//void print(const int *const arr, size_t array_size) // alternative 
+// This function expects:
+// arr - constant pointer pointing to constant int
+// so we cant change the pointer and what it is pointing to 
+//  *const arr and const int arr[] are same as in C++, when array[] is passed in function, its the
+// address of first element, and in case of *const int arr, pointer stores and address. 
+void print(const int array[], size_t array_size){ // const so that function doesnt modify the array
   std::cout << "[ "; 
   for (size_t i{}; i < array_size; ++i) 
-    std::cout << array[i] << " "; 
+    std::cout << array[i] << " "; // using array subscript notation 
+    // pointer offset notation *(arr + i)
   std::cout << "]" << std::endl; 
   return; 
 }
 
-int *apply_all(int arrayofinteger1[], size_t size1, int  arrayofinteger2[], size_t size2){
+/***********************************************************************************
+    The function dynamically allocates a new array that is of size = size1 * size2
+    Then it loops through each element of arr2 and multiples it across all the
+    elements of arr1 and each product is stored in the newly created array
+************************************************************************************/
+
+// int *apply_all(const int *const arrayofinteger1, size_t size1, const int *const arrayofinteger2, size_t size2) // alternative 
+/*
+  This function expects:
+      arrayofinteger1 - a pointer to an array of integers
+      size1 - the number of integers in arr1
+      arrayofinteger2 - a pointer to another array of integers
+      size2 - the number of integers in arr2
+*/
+int *apply_all(const int arrayofinteger1[], size_t size1, const int  arrayofinteger2[], size_t size2){
   // dynamic memory allocation 
   int *array_ptr {nullptr}; 
-  array_ptr = new int[size1 * size2];
+  array_ptr = new int[size1 * size2]; // allocating size1*size2 integers on the HEAP
 
   size_t index {}; 
   for (size_t i{}; i < size2; ++i)
     for (size_t j{}; j < size1; ++j){
       array_ptr[index] = *(arrayofinteger2 + i) * *(arrayofinteger1 + j); // pointer offset notation
-      index++;
+      // array_ptr[index] = arrayofinteger2[i] * arrayofinteger1[j]; // array subscript notation
+      ++index;
     }
   
-  return array_ptr; // return address of first element (so returing a pointer)
+  return array_ptr; // return address of first element of newly allocated space on the HEAP (so returing a pointer)
 } 
