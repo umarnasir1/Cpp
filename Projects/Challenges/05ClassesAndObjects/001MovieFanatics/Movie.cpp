69b8eda49ce9b9d01dce244549db8442d66dedbe
umarnasir1 <umar021@gmail.com>
2023-01-06 17:08:55 +0500

    Added comments.

--- Projects/Challenges/05ClassesAndObjects/001MovieFanatics/Movie.cpp
+++ Projects/Challenges/05ClassesAndObjects/001MovieFanatics/Movie.cpp
@@ -11,24 +11,23 @@
 #include <iostream>
 #include "Movie.h"
 
-// Implemention of the construcor
-
+// Implemention of the construcor as initilizer list
 Movie::Movie(std::string name, std::string rating, int watched) 
     : name(name), rating(rating), watched(watched)  {
 }
 
-//Implemention of the copy constructor
+//Implemention of the copy constructor (delegating to the constructor)
 Movie::Movie(const Movie &source) 
     : Movie{source.name, source.rating, source.watched} {
 }
 
-// Implementation of the destructor
+// Implementation of the destructor (doesnt do anything)
 Movie::~Movie() {
 }
 
 // Implementation of the display method
 // should just insert the movie attributes to cout
-
+// just displays the name, rating and watched for each of these objects. 
 void Movie::display() const {
     std::cout << name << ", " << rating <<  ", " << watched  <<   std::endl;
 }
