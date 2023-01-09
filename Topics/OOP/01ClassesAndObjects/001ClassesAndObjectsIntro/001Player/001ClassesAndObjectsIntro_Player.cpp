79e0e7d7c8169f54ad104bc58e1e2b85210a04d9
umarnasir1 <umar021@gmail.com>
2023-01-09 18:03:41 +0500

    Updated Comments.

--- Topics/OOP/01ClassesAndObjects/001ClassesAndObjectsIntro/001Player/001ClassesAndObjectsIntro_Player.cpp
+++ Topics/OOP/01ClassesAndObjects/001ClassesAndObjectsIntro/001Player/001ClassesAndObjectsIntro_Player.cpp
@@ -35,16 +35,15 @@ class Player {
 int main(){
   /* ANALOGY -OBJECTS
   We know that ints and doubles are primitive types in c++. They're not classes, but this analogy will help make a point
-
   Two integers, high_score and low_score are defined. This tells us what the valid values are for those variables (int), and it also tells us the operations we can perform on those variables, for example, addition, subtraction, multiplication and so forth. Notice that low score and high score are instances of integer. They each have a value, and they each have an identity.
-
   Again, integers are not classes. But just for a moment, think of int as the class and low score and high score as objects created from the int class.
   
   int high_score;
   int low_score;
-  */
 
-  // /*
+  Player frank; // frank is instance of player class
+  Player hero; // hero is instance of player class
+
   // The following lines create a scores object that's an instance of a vector of integers
   // and a name object that's an instance of string.
   // */
