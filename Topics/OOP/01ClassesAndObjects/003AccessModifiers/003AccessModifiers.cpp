175f3e9ecfa5df9b82f52a4146fa986f041c1b7f
umarnasir1 <umar021@gmail.com>
2023-01-10 14:26:14 +0500

    Added file in Folder.

---
+++ Topics/OOP/01ClassesAndObjects/003AccessModifiers/003AccessModifiers.cpp
@@ -0,0 +1,67 @@
+/*
+  Access Modifiers - Public, Private & Protected.
+*/
+#include<iostream>
+#include<string>
+
+using std::string;
+using std::cout;
+using std::endl;
+
+class Player {
+private: // only accessable from other members of this class and from friends of this class.
+  // attributes
+  string name {"Bjarne"};
+  int health;
+  int xp;
+
+public:
+  void talk(string text_to_say) {cout << name << " says: " << text_to_say << endl;}
+  bool is_dead();
+};
+
+class Account {
+  //attributes
+private:
+  string name;
+  double balance {20000.0};
+
+  // methods
+public: //Since these methods are member methods, they have access to the balance and can update its value.
+  bool deposit (double amount){balance += amount; cout << "Total Balance: " << balance << endl;}
+  bool withdraw (double amount){balance -= amount; cout << " Total Balance: " << balance << endl;}
+};
+
+int main (){
+
+  // Eg. 01
+  Player bjarne;
+
+  // What happend if we try to access a private class member
+  // bjarne.name = "Frank"; // Compiler error - name is private and is not accessable from outside the class
+  // bjarne.health = 1000; // Compiler error - name is private and is not accessable from outside the class
+  // cout << bjarne.health << endl; // Compiler error - as we are trying to use the variable from outside the class.
+  bjarne.talk("Ready to battle !!");
+
+  // Player *p1  = new int        // p1 points to an uninitialized integer
+  // Player *p2  = new int()      // p2 points to an integer initialized to 0
+  Player *enemy = new Player(); // () means initilized to zero here
+  // enemy->xp = 100; // Compiler error - xp is private and is not accessable from outside the class
+  enemy->talk("C++ - Object Oriented Programming" ); // OK
+
+  // Eg. 02
+  Account bjarne_account;
+
+  // bjarne_account.balance = 10000000.0; // compiler error - balance is private
+  bjarne_account.deposit(10000.0); // OK
+  // bjarne_account.name = "Bjarne's Account"; // compiler error - name is private
+
+  Account *stroustrup_account = new Account(); // () means initilized to zero
+
+  // stroustrup_account -> balance = 10000.0; // Compiler Error - balance is private
+  stroustrup_account -> withdraw(10000.0); // OK
+
+  delete stroustrup_account;
+
+  return 0;
+}
