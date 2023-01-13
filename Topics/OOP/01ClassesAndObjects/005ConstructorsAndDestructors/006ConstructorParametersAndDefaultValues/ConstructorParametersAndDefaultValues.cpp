20edf7db93a52614a994ffbf624d93dc0f1c2929
umarnasir1 <umar021@gmail.com>
2023-01-13 12:55:19 +0500

    Added file in folder.

---
+++ Topics/OOP/01ClassesAndObjects/005ConstructorsAndDestructors/006ConstructorParametersAndDefaultValues/ConstructorParametersAndDefaultValues.cpp
@@ -0,0 +1,34 @@
+/*
+  Constructor Parameters & Default Values
+  Ambigous Constructor - If we implement a no args constructor, we'd get a compiler error because the compiler wouldn't know which one to call.
+*/
+#include<iostream>
+#include<string>
+
+class Player{
+private:
+  std::string name;
+  int health;
+  int xp;
+public:
+  // Constructor with default parameter values (constrcutor declaration)
+  Player (std::string name_val = "None", // this constructor can replace 3 overloaded constructors in the previous eg.
+          int health_val = 0,
+          int xp_val = 0);
+};
+
+// constructor implimentation - Initilization List
+Player::Player (std::string name_val, int health_val, int xp_val)
+  : name {name_val}, health{health_val}, xp{xp_val} {
+    std::cout <<"Three-args constructor." << std::endl;
+}
+
+int main(){
+
+  Player empty; // None, 0, 0 - all three default parameters will be substituted.
+  Player frank {"Frank"}; // Frank,0,0
+  Player hero {"Hero", 100}; // Hero, 100, 0
+  Player bjarne {"Bjarne", 100, 55}; // Bjarne, 100, 55
+
+  return 0;
+}
