55ca9ad7f6fa9199666e13b20fa86bc41ad1f9c5
umarnasir1 <umar021@gmail.com>
2023-02-08 16:28:19 +0500

    Added in Folder.

---
+++ Topics/OOP/01ClassesAndObjects/009StructVsClasses/StructvsClasses.cpp
@@ -0,0 +1,30 @@
+/*
+  Struct vs Classes
+  members of struct are public by default wheras, members of class are private by default.
+*/
+#include<iostream>
+#include<string>
+
+// class Person{
+//   std::string name;
+//   std::string get_name(){ return name;}
+// };
+//
+// int main(){
+//   Person p;
+//   p.name = "Frank"; // ERROR - class memebers are private by default and we cannot access private members.
+//   std::cout << p.get_name(); // ERROR - class memebers are private by default and we cannot access private members.
+//   return 0;
+// }
+
+struct Person{
+  std::string name;
+  std::string get_name(){ return name;} // best practice is not to declare methods in structure. Here, its to show that we can put methods in structs.
+};
+
+int main(){
+  Person p;
+  p.name = "Frank"; // OK - as struct memebrs are public by default
+  std::cout << p.get_name(); // OK - as struct memebrs are public by default
+  return 0; 
+}
