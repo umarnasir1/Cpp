f7eb518a7322dd6053965ef601930f84d5e7e6b9
umarnasir1 <umar021@gmail.com>
2023-02-21 08:45:34 +0500

    Initial commit.

---
+++ Topics/Intermediate/01OperatorOverloading/003OverloadingMoveAssignmentOperator/main.cpp
@@ -0,0 +1,30 @@
+/*
+  Overloading move constructor and move assignment operator 
+
+  Mystring.h - class specification
+  Mystring.cpp - class definition
+  main.cpp
+
+  */
+#include <iostream>
+#include <vector>
+#include "Mystring.h"
+
+int main(){
+
+  Mystring a{"Hello"}; //Overloaded constructor
+  a = Mystring{"Hejsan"}; // Overloaded constructor then movee assignment 
+  a = "Tja"; // Overloaded constructor then move assignment 
+  
+  // objects
+  Mystring empty; // empty Mystring object - no-args constructor
+  Mystring larry {"Larry"}; // overloaded constructor used to initialize to string Larry (C-Style string literal. C++ compiler will treat it as a const char * and what is being passed to the constructor is the address of the first character in the null-terminated string literal.)
+  Mystring stooge {larry}; // copy constructor as we are initializing using larry object
+
+  // displaying objects
+  empty.display();
+  larry.display();
+  stooge.display();
+
+  return 0;
+}
