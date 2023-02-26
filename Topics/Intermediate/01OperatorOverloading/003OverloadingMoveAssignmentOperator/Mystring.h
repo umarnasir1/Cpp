515a7e2167d1dd41fb3b7e4fa84829c6a0d26817
umarnasir1 <umar021@gmail.com>
2023-02-26 23:59:41 +0500

    Added prototype for Copy and Move Assignment.

--- Topics/Intermediate/01OperatorOverloading/003OverloadingMoveAssignmentOperator/Mystring.h
+++ Topics/Intermediate/01OperatorOverloading/003OverloadingMoveAssignmentOperator/Mystring.h
@@ -29,7 +29,12 @@ public:
   Mystring(); // No-args constructor
   Mystring (const char *s); // Constructor that expects a c-style string (char) - Overloaded constructor
   Mystring (const Mystring &source); // copy constructor
+  Mystring (Mystring &&source); // Move constructor
   ~Mystring(); // Destructor
+
+  Mystring &operator=(const Mystring &rhs); // Copy assignmnet 
+  Mystring &operator=(Mystring &&rhs); // Move assignment
+
   void display() const; // displays the contents string (string and its length)
   int get_length() const; // getter - returns current length of the string
   const char *get_str() const; // getter - returns pointer to the string (as a const)
