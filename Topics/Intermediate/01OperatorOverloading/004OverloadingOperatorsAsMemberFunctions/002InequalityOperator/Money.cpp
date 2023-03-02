32ffa97b90b4a3686eea2ef3ce51f87183ee87dd
umarnasir1 <umar021@gmail.com>
2023-03-02 10:59:07 +0500

    Initial commit.

---
+++ Topics/Intermediate/01OperatorOverloading/004OverloadingOperatorsAsMemberFunctions/002InequalityOperator/Money.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include "Money.h"
+
+Money::Money(int dollars, int cents) 
+    :dollars{dollars}, cents{cents} {
+}
+
+Money::Money(int total) 
+    :dollars {total/100}, cents{total%100} {
+}
+
+bool Money::operator== (const Money &rhs) const{
+    return ( dollars == rhs.dollars && cents == rhs.cents );
+}
+
+bool Money::operator!= (const Money &rhs) const{
+    return !(rhs == *this); // using == operator in this method. Is it because you declared == operator overloading before
+}
+
+/*
+    we can use     return ( dollars != rhs.dollars && cents != rhs.cents ) but better approach is return !(rhs == *this)
+
+    since we already implemented operator== and it returns a boolean indicating if the objects are the 'same' or not.
+    Then we can simple negate that result for !=
+    !(rhs == *this)
+    become ! (rhs.operator==(*this))
+    which compare the two objects rhs and *this using operator== and then negates the result.
+*/
