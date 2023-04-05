1c0fe02315b6ac1cbd1017db2bda5e1cd3ee8f0b
umarnasir1 <umar021@gmail.com>
2023-04-05 23:39:48 +0500

    (1) Added Class Item; (2) Updated comments.

--- Topics/Intermediate/07TheStandardTemplateLibrarySTL/004GenericProgrammingWithClassTemplates/GenericProgrammingWithClassTemplates.cpp
+++ Topics/Intermediate/07TheStandardTemplateLibrarySTL/004GenericProgrammingWithClassTemplates/GenericProgrammingWithClassTemplates.cpp
@@ -1,8 +1,39 @@
 /*
   Generic Programming with Class Templates
 
+  How to make a class generic
+    Class to hold items: string (name) and an integer (value).
+    Instead of an int and a string, we want to model a double and a string.
+
 */
 #include<iostream>
+
+// class Item{
+// private:
+//   std::string name; 
+//   int value; 
+// public: 
+//   Item(std::string name, int value) // 2 args constructor
+//     :name{name}, value{value}{
+//   }
+//   // getters
+//   std::string get_name() const {return name;}
+//   int get_value() const {return value;}
+// };
+
+template <typename T> 
+class Item{
+private:
+  std::string name; 
+  T value; 
+public: 
+  Item(std::string name, T value) // 2 args constructor
+    :name{name}, value{value}{
+  }
+  // getters
+  std::string get_name() const {return name;}
+  T get_value() const {return value;}
+};
 
 int main (){
 
