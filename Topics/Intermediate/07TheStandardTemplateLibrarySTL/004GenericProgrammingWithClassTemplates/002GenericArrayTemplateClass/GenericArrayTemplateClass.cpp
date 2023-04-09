2c7047a2c79296e6b1f637a6354c651e25d60ed0
umarnasir1 <umar021@gmail.com>
2023-04-09 07:58:12 +0500

    Initial Commit.

---
+++ Topics/Intermediate/07TheStandardTemplateLibrarySTL/004GenericProgrammingWithClassTemplates/002GenericArrayTemplateClass/GenericArrayTemplateClass.cpp
@@ -0,0 +1,12 @@
+/*
+    Generic Programming with Class Templates
+        Creating a Generic Array Template class (Class Template for Array class)
+*/
+
+#include<iostream>
+#include<string>
+
+class Array{
+    int size{N};
+    int values{N};
+};
