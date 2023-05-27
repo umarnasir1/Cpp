37a048772fcfc92c316f69bf27baf4b8fc661e27
umarnasir1 <umar021@gmail.com>
2023-05-27 17:21:36 +0500

    Orders algos in alphabetical order.

--- Topics/Intermediate/07TheStandardTemplateLibrarySTL/001STLAlgorithms/001STLAlgorithms.cpp
+++ Topics/Intermediate/07TheStandardTemplateLibrarySTL/001STLAlgorithms/001STLAlgorithms.cpp
@@ -1,11 +1,11 @@
 /*
   STL Algorithm:
-    sort() - sorting in ascending order.
-    reverse() - sorting in descending order.
     accumulate () - sum of the elements in the vector
     copy ()
       back_inserter
     min (), max () - check later
+    sort() - sorting in ascending order.
+    reverse() - sorting in descending order.
 
     TODO:
       Test with providing subset of vector.
@@ -24,6 +24,22 @@ int main (){
   for (auto elem: v)
     std::cout << elem << " "; 
 
+  // Accumulate
+  std::cout << "\nThe accumulation of vector elements is: " << std::accumulate(v.begin(), v.end(), 0) << std::endl; // 0 - running sum will start at; if double then 0.0; typically 0, but we can use any number. If we use 3 then the accumulation will start from 3 onwards.
+  
+  // Copy
+  std::vector<int> v2 { 4, 5, 2, 8, 0, 9, 2}; 
+  std::copy(v.begin(), v.end(), std::back_inserter (v2));
+  std::cout<< "\nAfter Copy: "; 
+  for (auto elem: v2)
+    std::cout << elem << " "; // After Copy: 4 5 2 8 0 9 2 10 20 5 23 42 15
+
+  // // max
+  // std::cout << "\nMaximum element of vector is:  " << *max_element(v.begin(), v.end()); 
+
+  // // min
+  // std::cout << "\nMinimum element of vector is:  " << *min_element(v.begin(), v.end());
+  
   // sort a vector
   std::cout << "\nVector after sorting is: "; 
   std::sort(v.begin(), v.end()); // the entire vector
@@ -36,22 +52,6 @@ int main (){
   std::reverse(v.begin(), v.end());
   for (auto elem_r: v)
     std::cout << elem_r << " ";
-
-  // // max
-  // std::cout << "\nMaximum element of vector is:  " << *max_element(v.begin(), v.end()); 
-
-  // // min
-  // std::cout << "\nMinimum element of vector is:  " << *min_element(v.begin(), v.end());
-  
-  // Accumulate
-  std::cout << "\nThe accumulation of vector elements is: " << std::accumulate(v.begin(), v.end(), 0) << std::endl; // 0 - running sum will start at; if double then 0.0; typically 0, but we can use any number. If we use 3 then the accumulation will start from 3 onwards.
-
-  // Copy
-  std::vector<int> v2 { 4, 5, 2, 8, 0, 9, 2}; 
-  std::copy(v.begin(), v.end(), std::back_inserter (v2));
-  std::cout<< "\nAfter Copy: "; 
-  for (auto elem: v2)
-    std::cout << elem << " "; // After Copy: 4 5 2 8 0 9 2 42 23 20 15 10 5 
 
   std::cout << std::endl; 
   return 0;
