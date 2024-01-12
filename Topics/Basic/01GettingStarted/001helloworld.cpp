2740e762acfdad2b611aacd7cf3aefd66bfe00ae
umarnasir1 <umar021@gmail.com>
2024-01-12 08:54:41 +0500

    Spelling Change

--- Topics/Basic/01GettingStarted/001helloworld.cpp
+++ Topics/Basic/01GettingStarted/001helloworld.cpp
@@ -1,6 +1,6 @@
 #include <iostream> // input, output libraries for e.g., cout
 
 int main() {
-  std::cout << "Hellow World!!" << std::endl; //endl will flush the buffers and will move the cursor to the next line. If stream is buffered, it may not get written to the console until it is flushed
+  std::cout << "Hello World!!" << std::endl; //endl will flush the buffers and will move the cursor to the next line. If stream is buffered, it may not get written to the console until it is flushed
   return 0;
 }
