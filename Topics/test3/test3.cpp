69d3af35747d1bb2e2d39fa3d95a85b7c367b0ea
umarnasir1 <umar021@gmail.com>
2025-01-12 07:02:47 +0500

    firt commit

---
+++ Topics/test3/test3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main (){
+
+    int side {};
+
+    std::cout << "Enter the lenght of a side of a square: ";
+    std::cin >> side;
+
+    std::cout << "The area of the square is: " << side*side << std::endl;
+
+    return 0;
+}
