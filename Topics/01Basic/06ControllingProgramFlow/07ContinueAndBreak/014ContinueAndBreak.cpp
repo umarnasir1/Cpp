a6587c1ba63c694f793e64954ab2153ee604f7ce
umarnasir1 <umar021@gmail.com>
2025-01-20 10:25:50 +0500

    update

--- Topics/01Basic/06ControllingProgramFlow/07ContinueAndBreak/014ContinueAndBreak.cpp
+++ Topics/01Basic/06ControllingProgramFlow/07ContinueAndBreak/014ContinueAndBreak.cpp
@@ -23,3 +23,22 @@ int main (){
 
   return 0;
 }
+
+/* we can not use the continue and use 'else if (val == -1) ;' instead .. that will work too 
+int main (){
+
+  std::vector <int> values {1,2,-1,3,-1,-99,7,8,10}; // This data is comming from a device/sensor and occasionally we get noise in the data.
+  // -1 represents noise & we dont want to process the noise and we want to ignore it.
+  // -99 stop signal, we need to stop processing when we get to stop signal.
+
+  for (auto val: values)
+    if (val == -99)
+      break; // break out of the loop as stop signal
+    else if (val == -1) ;// noise case
+      //continue; //This stops the iteration and control goes back to the beginning of the loop for the next iteration.
+    else
+      std::cout << val << std::endl; // process data
+
+  return 0;
+}
+*/
