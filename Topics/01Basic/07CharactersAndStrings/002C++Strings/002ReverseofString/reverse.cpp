99cfb341743799605f9a24c731e784350a9e6e04
umarnasir1 <umar021@gmail.com>
2025-01-29 10:56:26 +0500

    update

--- Topics/01Basic/07CharactersAndStrings/002C++Strings/002ReverseofString/reverse.cpp
+++ Topics/01Basic/07CharactersAndStrings/002C++Strings/002ReverseofString/reverse.cpp
@@ -1,4 +1,4 @@
-/* Write a C++ Program to revrse a string */
+/* Write a C++ Program to revrse a string 
 
 #include<iostream>
 #include<string>
@@ -9,22 +9,22 @@ int main (){
     std::cout << "Enter a string: ";
     getline(std::cin,s1);
 
-    for (size_t i{s1.size()-1}; i >= 0; --i)  // some problem in the loop, instead of i going to 0, its going to  18446744073709551615
-        std::cout << s1.at(i);
+    for (size_t i{s1.size()-1}; i >= 0; --i)  // some problem in the loop, instead of i going to -1 instead of 0, its going to  18446744073709551615
+        std::cout << s1.at(i); // if do it as a code, then why error if using at code.
 
     std::cout << std::endl;
 
     return 0;
-} 
+} */
 
 
-/*3. Write a C++ program to reverse a string 
+//3. Write a C++ program to reverse a string 
 #include <iostream>
 #include <string>
 
 int main()
 {
-    std::string var1{};
+    std::string var1;
 
     std::cout << "enter a string :";
     getline(std::cin, var1);
@@ -37,4 +37,3 @@ int main()
     std::cout << std::endl;
     return 0;
 }
-*/
