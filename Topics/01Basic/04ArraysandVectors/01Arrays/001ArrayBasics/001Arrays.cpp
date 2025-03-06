8976710223a08d2988e4353b6294bafff46f621b
umarnasir1 <umar021@gmail.com>
2025-03-06 20:05:13 +0500

    update

--- Topics/01Basic/04ArraysandVectors/01Arrays/001ArrayBasics/001Arrays.cpp
+++ Topics/01Basic/04ArraysandVectors/01Arrays/001ArrayBasics/001Arrays.cpp
@@ -1,5 +1,13 @@
 /*
   Arrays
+  These are C++ Raw Arrays 
+
+  Since C++11 the STL has std::array which is a template-based array class.
+    Usage: When we know exactly what we have e.g., we know that we have 10 integers, or 20 integers or 5 strings. 
+      (In this case, the vector might be overkill, and it could be a little less efficient. In a case like that, we really want to 
+      use std::array. E.g., 12 months in a year, that will not change so in this case we will use std::array).
+    OBS!! In modern C++, it is recommended to to use std::array instead of raw arrays.
+    supports algorithms, iterators
 */
 
 #include <iostream>
