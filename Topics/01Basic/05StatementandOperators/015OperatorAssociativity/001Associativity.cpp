9b23663d24b42903b8bdb2baf51300ee07ead6b3
umarnasir1 <umar021@gmail.com>
2025-03-28 10:54:32 +0500

    Added Ex4.

--- Topics/01Basic/05StatementandOperators/015OperatorAssociativity/001Associativity.cpp
+++ Topics/01Basic/05StatementandOperators/015OperatorAssociativity/001Associativity.cpp
@@ -15,11 +15,17 @@ int main(){
     // - amd + have same presidence. so associativity is left to right (-8 + 10 )
 
     // Ex3
-    total = var2++ - var3-- + var1; // 0 
+    // total = var2++ - var3-- + var1; // 0 
     // Uniary operators have higher presidence so will perform ++ and -- first. 
     // ++ and -- have same presidence, so associativity is right to left so -- will be performed first and then  ++, but as it is post 
     //   increment/decrement, the value of var3 and var2 will be decremented/incremented later (after evaluation of the statement).  ( 20 - 30 + 10)
     // - amd + have same presidence. so associativity is left to right (-10 + 10 )
+
+    // Ex4
+    total = var2++ - (var3+ var1); // -20;
+    // Brackets will have higher precision 
+    // After it the post increment operator will be solved but the value will be increment after the expression is completely evaluated
+    //   (20++ - 40) -> (20 - 40)
 
     std::cout << total << std::endl;
 
