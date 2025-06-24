3abd330aa11dd65996db4971ba8b9c44fce24a41
umarnasir1 <umar021@gmail.com>
2025-06-24 10:10:02 +0500

    Adding test to check std::format support.

---
+++ Topics/04Others/001stdformatSupport/working.cpp
@@ -0,0 +1,19 @@
+/* To check if 
+ updated 2025-06-24
+ 
+ std::format is new feature of c++20
+ if we want to use this format library download and install from reference implimentation
+
+ https://fmt.dev/latest/index.html
+
+ conclusion: std::format is not supported yet
+ */
+#include <format>
+#include <iostream>
+
+using std::format; // not working till now 
+using std::cout;
+
+int main() {
+    cout << "Hello, World!\n";
+}
