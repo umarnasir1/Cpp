5cbc5dfb699dba922c0533c88184b653b07f1647
umarnasir1 <umar021@gmail.com>
2025-06-28 23:41:40 +0500

    Added one example.

---
+++ Topics/04Others/001stdformat/stdformat.cpp
@@ -0,0 +1,36 @@
+/* To check if 
+ updated 2025-06-24
+ 
+ std::format is new feature of c++20
+ if we want to use this format library download and install from reference implimentation
+
+ https://fmt.dev/latest/index.html
+
+ conclusion: std::format is not supported yet
+ We will be using format to format strings in this course. 
+ */
+#include <cstdio>
+#include <format> // C++20
+#include <iostream>
+
+int main() {
+
+    // Eg. 1
+    int i {};
+    std::cout << std::format("i is {}\n", i);
+
+    // Eg. 2
+    const char* str {"Hello, World!"};
+    std::cout << std::format("The str is {}\n", str); // this will give error as std::format is not working
+
+    // Eg. 3
+    int x {42};
+    auto str = std::format("x is {}\n", x);
+    std::cout << str;
+
+    //
+    std::cout << std::format("Element {} is {}\n", i, array[i]); 
+    std::cout << std::format("The value of x is {}\n", x);
+    std::cout << std::format("The value of y is {}\n", y);
+    std::cout << std::format("The value of *ip is {}\n", *ip);
+}
