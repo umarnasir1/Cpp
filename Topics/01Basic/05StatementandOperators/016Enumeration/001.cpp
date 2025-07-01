22ee7875b668f0d867dec8269a02646ebe190b44
umarnasir1 <umar021@gmail.com>
2025-07-01 20:41:15 +0500

    Updated comments.

--- Topics/01Basic/05StatementandOperators/016Enumeration/001.cpp
+++ Topics/01Basic/05StatementandOperators/016Enumeration/001.cpp
@@ -1,11 +1,8 @@
 /*
   We have an enumeration type called color with 3 enumeration constants: red, green and blue. 
 
-  enum Color { // we are creating a new type
-    red, green, blue // enumeration constants
-  };
-
-   Color screen_color {green}; // create variable of enumeration type Color and initilizing it to green- (can only be red, green, blue)
+  enum Color { red, green, blue};// we are creating a new type with enumeration constants
+  Color screen_color {green}; // create variable of enumeration type Color and initilizing it to green- (can only be red, green, blue)
    // error if we initialize with any other then defined three.  
 
 */
