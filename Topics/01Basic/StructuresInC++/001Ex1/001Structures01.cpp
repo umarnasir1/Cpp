986a658a08657a941b4e683baf4f98812358eb36
umarnasir1 <umar021@gmail.com>
2025-08-03 07:09:00 +0500

    Accessing Members via pointer Example added.

--- Topics/01Basic/StructuresInC++/001Ex1/001Structures01.cpp
+++ Topics/01Basic/StructuresInC++/001Ex1/001Structures01.cpp
@@ -21,7 +21,14 @@ int main(){
 
   S s1 {23, 34.2, "Test"}; // initializer list to initilize variable or object based on that structure.
   // S - structure name; s1 - object name 
-  std::cout << "s1: "<< s1.i << ", " << s1.d << ", " << s1.s << std::endl;
-
+  std::cout << "s1: "<< s1.i << ", " << s1.d << ", " << s1.s << std::endl; // s1: 23, 34.2, Test
+  s1.d = 73.0; // writing value 
+  std::cout << "s1: "<< s1.i << ", " << s1.d << ", " << s1.s << std::endl; // s1: 23, 73, Test 
+  // accessing memebers of structure via pointer with ->
+  auto *sp {&s1};
+  sp->d = 75.9; // writing value via pointer member defrence operator which accesses a member through a pointer
+  std::cout << "s1: "<< s1.i << ", " << s1.d << ", " << s1.s << std::endl; // s1: 23, 75.9, Test
+  std::cout << "s1: "<< sp->i << ", " << sp->d << ", " << sp->s << std::endl; // s1: 23, 75.9, Test
+  
   return 0;
 }
