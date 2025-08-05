4221c7e45c7716280d2075b556a7f7777b748c4d
umarnasir1 <umar021@gmail.com>
2025-08-05 13:19:48 +0500

    Initial Commit.

---
+++ Projects/Challenges/Operators/PrimeNumbers.cpp
@@ -0,0 +1,7 @@
+/*
+    Write a program to calculate the first 25 prime numbers, which are all the prime numbers under 100.
+    (2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97)
+
+    Prime Number Definition: A prime number is a natural number greater than 1 that cannot be formed by multiplying two smaller natural numbers, 
+    i.e., its only factors are one and itself.
+*/
