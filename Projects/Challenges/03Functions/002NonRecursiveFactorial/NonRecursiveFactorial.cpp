e8305837174613d2f9aa297901520eb79e6e81e9
umarnasir1 <umar021@gmail.com>
2025-08-18 14:10:17 +0500

    Adding comment about non-recursive factorial implimentation.

--- Projects/Challenges/03Functions/002NonRecursiveFactorial/NonRecursiveFactorial.cpp
+++ Projects/Challenges/03Functions/002NonRecursiveFactorial/NonRecursiveFactorial.cpp
@@ -5,13 +5,31 @@
     Challenge Goal: Write a function that takes a value and returns its factorial without using recursion.
 */
 
+#include <iostream>
+
 unsigned long long factorial (unsigned long long n); 
 
 int main (){
+    unsigned long long num{0};
+    std::cout << "Enter the number: "; 
+    std::cin >> num; 
+    std::cout << "The factorial of " << num << " is " << factorial(num) << std::endl;
     return 0; 
 }
 
 unsigned long long factorial (unsigned long long n){
+    auto fac{n};
 
-    
+    do{
+        fac *= --n; 
+    }while(n>1);
+
+    return fac;
 }
+
+// Alternative with while 
+/*
+    while (n>1){
+        fac *= --n; 
+    }
+*/
