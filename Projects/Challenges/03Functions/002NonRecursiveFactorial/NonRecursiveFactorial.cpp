51f7008840cc55021c0c1a827471d0131fe4dbbb
umarnasir1 <umar021@gmail.com>
2025-08-18 12:11:54 +0500

    Initial commit.

---
+++ Projects/Challenges/03Functions/002NonRecursiveFactorial/NonRecursiveFactorial.cpp
@@ -0,0 +1,17 @@
+/*
+    Objective: Create a non-recursive function to calculate factorials, avoiding the issues of stack overflow that can occur 
+    with recursive functions.
+
+    Challenge Goal: Write a function that takes a value and returns its factorial without using recursion.
+*/
+
+unsigned long long factorial (unsigned long long n); 
+
+int main (){
+    return 0; 
+}
+
+unsigned long long factorial (unsigned long long n){
+
+    
+}
