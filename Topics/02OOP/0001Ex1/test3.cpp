d1d11095dbec048ce5b1cdb33a4d1f3157660c36
umarnasir1 <umar021@gmail.com>
2025-10-29 16:23:45 +0500

    Initial Commit.

---
+++ Topics/02OOP/0001Ex1/test3.cpp
@@ -0,0 +1,21 @@
+/*
+  Best Practice to seperate interface & implimentation
+  setvalue() & getvalue() also called function accesors 
+*/
+
+#include <iostream>
+
+class c1{ // Class definition
+  private: // even if we dont write private, it will be private by default
+    int value {}; 
+  public: 
+    c1 // Constructor used to initialize private data memebr 
+    void setvalue (int number) {value = number; }
+    int getvalue () {return value; }
+};
+
+int main (){
+  c1 o1; 
+  o1.setvalue(92);
+  std::cout<< "The value is : " << o1.getvalue() << std::endl; 
+}
