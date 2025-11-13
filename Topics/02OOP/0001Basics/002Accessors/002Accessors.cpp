13ca0216ba5b83e4e4c23adee3584a83ca31619c
umarnasir1 <umar021@gmail.com>
2025-11-13 07:41:41 +0500

    Updated.

---
+++ Topics/02OOP/0001Basics/002Accessors/002Accessors.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+class C1 { // default data memebers are private 
+        int num1 {}; // cannot initialize here in classes
+        int num2 {}; 
+        int num3 {};    
+    public:
+        C1 (int a, int b, int c) : num1{a}, num2{b}, num3{c} {} // Constructor Initialization List
+        // setters 
+        void setnum1 (int a) { num1 = a; } 
+        void setnum2 (int b) { num2 = b; }
+        void setnum3 (int c) { num3 = c; }
+        // getters 
+        int getnum1 () {return num1; }
+        int getnum2 () {return num2; }
+        int getnum3 () {return num3; }
+};
+
+int main (){
+    C1 o1 {47, 37, 85}; // creating object and initializing via objects (constuctor initialization list)
+    // std::cout << o1.num1 << " " << o1.num2 << " " << o1.num3 << std::endl; // error as num1, num2, num3 are private
+    std::cout << o1.getnum1() << " " << o1.getnum2() << " " << o1.getnum3() << std::endl;
+    o1.setnum1(55); // setting num1 
+    o1.setnum2(80);
+    o1.setnum3(99);
+    std::cout << o1.getnum1() << " " << o1.getnum2() << " " << o1.getnum3() << std::endl;
+    return 0; 
+}
