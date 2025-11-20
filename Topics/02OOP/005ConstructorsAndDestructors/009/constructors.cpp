a1de94bc222042b445c832f9ef3833bed7368b18
umarnasir1 <umar021@gmail.com>
2025-11-20 11:23:14 +0500

    Updated comments.

--- Topics/02OOP/005ConstructorsAndDestructors/009/constructors.cpp
+++ Topics/02OOP/005ConstructorsAndDestructors/009/constructors.cpp
@@ -1,36 +1,40 @@
+/*
+    Checking Constructor Calls
+*/
 #include <iostream>
 #include <string>
 
-const std::string unk {"unknown"};
-const std::string clone_prefix {"clone-"};
+// In static memory space bec they are outside of any function
+const std::string unk {"unknown"}; 
+const std::string clone_prefix {"clone-"}; // simple prefix string 
 
-// -- interface --
+// -- Class Interface --
 class Animal {
     std::string a_type {};
     std::string a_name {};
     std::string a_sound {};
 public:
-    Animal();   // default constructor
-    Animal(const std::string& type, const std::string& name, const std::string& sound);
-    Animal(const Animal&);  // copy constructor
+    Animal(); // default constructor - initialize the object to default state 
+    Animal(const std::string &type, const std::string &name, const std::string &sound); // Constructor with args 
+    Animal(const Animal&);  // copy constructor - takes data from another animal object
     ~Animal();  // destructor
-    Animal& operator = (const Animal&);   // copy/assignment operator
-    void print() const;
+    Animal& operator = (const Animal&);   // copy/assignment operator - acts like a constructor - overloading = operator
+    void print() const; // to print objects ; const qualified function to work with const objects 
 };
 
 // -- implementation --
-Animal::Animal() : a_type(unk), a_name(unk), a_sound(unk) {
+Animal::Animal() : a_type(unk), a_name(unk), a_sound(unk) { //initializer list, unk - unknown defined as constant
     std::cout << "default constructor" << std::endl;
 }
 
-Animal::Animal(const std::string& type, const std::string& name, const std::string& sound)
-: a_type(type), a_name(name), a_sound(sound) {
+Animal::Animal(const std::string &type, const std::string &name, const std::string &sound) // const string references 
+: a_type(type), a_name(name), a_sound(sound) { // initializer list
     std::cout << "constructor with arguments" << std::endl;
 }
 
-Animal::Animal(const Animal& a) {
+Animal::Animal(const Animal &a) { // const reference to another object 
     std::cout << "copy constructor" << std::endl;
-    a_name = clone_prefix + a.a_name;
+    a_name = clone_prefix + a.a_name; // clone prefix being added to the beginning of the name using addition operator from the string class
     a_type = a.a_type;
     a_sound = a.a_sound;
 }
@@ -39,14 +43,14 @@ Animal::~Animal() {
     std::cout << "destructor: " << a_name << " the " << a_type << std::endl;
 }
 
-Animal& Animal::operator = (const Animal& o) {
+Animal& Animal::operator = (const Animal &o) { // Overloading assignment operator
     std::cout << "assignment operator" << std::endl;
-    if(this != &o) {
+    if(this != &o) { // checking on safe side; condition to check if we are not copying myself into myself. 
         a_name = clone_prefix + o.a_name;
         a_type = o.a_type;
         a_sound = o.a_sound;
     }
-    return *this;
+    return *this; // copy assignment operator always returns a reference to this, and so we dereference the pointer and use reference operator (Animmal&) to return reference to our current object. 
 }
 
 void Animal::print () const {
@@ -54,16 +58,17 @@ void Animal::print () const {
 }
 
 int main() {
-    Animal a {};
+    Animal a {}; // default construtor has set the class to a default state, object = a;
     a.print();
     
-    const Animal b("goat", "bob", "baah");
+    const Animal b("goat", "bob", "baah"); // constructor with argruments; constant object b;
     b.print();
     
-    const Animal c = b;
+    const Animal c {b}; // copy constructor; - no need of operator overloading =
+    //const Animal c = b; // can use assignment operator also for copy constructor; - no need of operator overloading = 
     c.print();
     
-    a = c;
+    a = c; // assignment operator will be overloaded
     a.print();
     
     std::cout << "end of main\n";
