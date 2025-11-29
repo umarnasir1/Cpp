74d82c36650b1cbaa18e9093085b6d77d563d827
umarnasir1 <umar021@gmail.com>
2025-11-29 08:44:21 +0500

    (1) Updated Examples.

--- Topics/04Others/001stdformat/stdformat.cpp
+++ Topics/04Others/001stdformat/stdformat.cpp
@@ -1,40 +1,38 @@
-/* To check if 
- updated 2025-06-24
+/* 
  
  std::format is new feature of c++20
- if we want to use this format library download and install from reference implimentation
-
- https://fmt.dev/latest/index.html
-
- conclusion: std::format is not supported yet
- We will be using format to format strings in this course. 
 
  format() returns a string.
  */
-#include <cstdio>
 #include <format> // C++20
 #include <iostream>
+#include <string>
 
 int main() {
 
     // Eg. 1
     int i {};
-    std::cout << std::format("i is {}\n", i);
+    std::cout << std::format("i is {}\n", i); //i is 0
 
     // Eg. 2
-    const char* str {"Hello, World!"};
-    std::cout << std::format("The str is {}\n", str); // this will give error as std::format is not working
+    const char* str {"Hello, World!"}; // C-Style string
+    std::cout << std::format("The str is {}\n", str); // The str is Hello, World!
 
     // Eg. 3
-    int x {42};
-    auto str = std::format("x is {}\n", x);
-    std::cout << str;
+    int x {42}, y {50};
+    std::cout << std::format("The value of x is {}\n", x); // The value of x is 42
+    std::cout << std::format("The value of y is {}\n", y); // The value of y is 50
+    
+    // define an array 
+    //std::cout << std::format("Element {} is {}\n", i, array[i]); 
 
-    //
-    std::cout << std::format("Element {} is {}\n", i, array[i]); 
-    std::cout << std::format("Element {} is {}\n", i, string[i]);
-    std::cout << std::format("The value of x is {}\n", x);
-    std::cout << std::format("The value of y is {}\n", y);
-    std::cout << std::format("The value of *ip is {}\n", *ip);
-    std::cout << std::format("Value of df is {:.20f}\n", df); // displaying precision of double - // 20 decimal places 
+    // define a string
+    //std::cout << std::format("Element {} is {}\n", i, string[i]);
+    
+    // define a pointer 
+    //std::cout << std::format("The value of *ip is {}\n", *ip);
+
+    // e.g. of decimal place
+    //std::cout << std::format("Value of df is {:.20f}\n", df); // displaying precision of double - // 20 decimal places 
+    
 }
